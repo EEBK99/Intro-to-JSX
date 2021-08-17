04976621ed4b9651d11d4fe4bedebc01d343d7c3
Muhammad Haseeb Farooq Khan <49371450+EEBK99@users.noreply.github.com>
2021-08-17 15:12:52 +0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Intro-to-JSX
+# introduction-to-jsx
 Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import react from "react";
+import reactDOM from "react-dom";
+
+var name = "Haseeb Khan";
+var date = Date.now();
+
+reactDOM.render();
